705cfb4c075b62f51315ccb129d164f864f4a629
Ronald Gosso <ronald.gosso@neptuneprolabs.com>
2026-03-31 13:41:27 +0300

    feat: initialize package structure and expose core components in __init__.py

--- topscrape/__init__.py
+++ topscrape/__init__.py
@@ -11,7 +11,7 @@ from topscrape.exceptions import (
 from topscrape.fields import Field
 from topscrape.models import ScraperModel
 
-__version__ = "0.1.0"
+__version__ = "0.1.3"
 __all__ = [
     "ScraperModel",
     "Field",
